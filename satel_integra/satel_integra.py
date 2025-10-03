ecf267188c3e502e0bf94979c7dd8274111a06d8
Tom Matheussen <tmatheussen@avia-gis.com>
2025-10-03 15:25:53 +0000

    Initial support for structured commands added

--- satel_integra/satel_integra.py
+++ satel_integra/satel_integra.py
@@ -1,10 +1,10 @@
-# -*- coding: utf-8 -*-
-
 """Main module."""
 
 import asyncio
 import logging
 from enum import Enum, unique
+
+from .command import SatelReadCommand, SatelResultCode, SatelWriteCommand
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -30,19 +30,21 @@ def print_hex(data):
 
 def verify_and_strip(resp):
     """Verify checksum and strip header and footer of received frame."""
-    if resp[0:2] != b'\xFE\xFE':
+    if resp[0:2] != b"\xfe\xfe":
         _LOGGER.error("Houston, we got problem:")
         print_hex(resp)
-        raise Exception("Wrong header - got %X%X" % (resp[0], resp[1]))
-    if resp[-2:] != b'\xFE\x0D':
-        raise Exception("Wrong footer - got %X%X" % (resp[-2], resp[-1]))
-    output = resp[2:-2].replace(b'\xFE\xF0', b'\xFE')
+        raise Exception(f"Wrong header - got {resp[0]:X}{resp[1]:X}")
+    if resp[-2:] != b"\xfe\x0d":
+        raise Exception(f"Wrong footer - got {resp[-2]:X}{resp[-1]:X}")
+    output = resp[2:-2].replace(b"\xfe\xf0", b"\xfe")
 
     c = checksum(bytearray(output[0:-2]))
 
     if (256 * output[-2:-1][0] + output[-1:][0]) != c:
-        raise Exception("Wrong checksum - got %d expected %d" % (
-            (256 * output[-2:-1][0] + output[-1:][0]), c))
+        raise Exception(
+            "Wrong checksum - got %d expected %d"
+            % ((256 * output[-2:-1][0] + output[-1:][0]), c)
+        )
 
     return output[0:-2]
 
@@ -55,7 +57,7 @@ def list_set_bits(r, expected_length):
     """
     set_bit_numbers = []
     bit_index = 0x1
-    assert (len(r) == expected_length + 1)
+    assert len(r) == expected_length + 1
 
     for b in r[1:]:
         for i in range(8):
@@ -72,7 +74,7 @@ def generate_query(command):
     c = checksum(data)
     data.append(c >> 8)
     data.append(c & 0xFF)
-    data.replace(b'\xFE', b'\xFE\xF0')
+    data.replace(b"\xfe", b"\xfe\xf0")
 
     data = bytearray.fromhex("FEFE") + data + bytearray.fromhex("FE0D")
     return data
@@ -81,17 +83,17 @@ def generate_query(command):
 def output_bytes(output):
     _LOGGER.debug("output_bytes")
     output_no = 1 << output - 1
-    return output_no.to_bytes(32, 'little')
+    return output_no.to_bytes(32, "little")
 
 
 def partition_bytes(partition_list):
-        ret_val = 0
-        for position in partition_list:
-            if position >= 32:
-                raise IndexError()
-            ret_val = ret_val | (1 << (position - 1))
+    ret_val = 0
+    for position in partition_list:
+        if position >= 32:
+            raise IndexError()
+        ret_val = ret_val | (1 << (position - 1))
 
-        return ret_val.to_bytes(4, 'little')
+    return ret_val.to_bytes(4, "little")
 
 
 @unique
@@ -114,13 +116,13 @@ class AlarmState(Enum):
 class AsyncSatel:
     """Asynchronous interface to talk to Satel Integra alarm system."""
 
-    def __init__(self, host, port, loop, monitored_zones=[],
-                 monitored_outputs=[], partitions=[]):
+    def __init__(
+        self, host, port, loop, monitored_zones=[], monitored_outputs=[], partitions=[]
+    ):
         """Init the Satel alarm data."""
         self._host = host
         self._port = port
         self._loop = loop
-        self._message_handlers = {}
         self._monitored_zones = monitored_zones
         self.violated_zones = []
         self._monitored_outputs = monitored_outputs
@@ -138,29 +140,41 @@ class AsyncSatel:
         self._command_status_event = asyncio.Event()
         self._command_status = False
 
-        self._message_handlers[b'\x00'] = self._zone_violated
-        self._message_handlers[b'\x17'] = self._output_changed
-        self._message_handlers[b'\x0A'] = lambda msg: self._armed(
-            AlarmState.ARMED_MODE0, msg)
-        self._message_handlers[b'\x2A'] = lambda msg: self._armed(
-            AlarmState.ARMED_MODE1, msg)
-        self._message_handlers[b'\x0B'] = lambda msg: self._armed(
-            AlarmState.ARMED_MODE2, msg)
-        self._message_handlers[b'\x0C'] = lambda msg: self._armed(
-            AlarmState.ARMED_MODE3, msg)
-        self._message_handlers[b'\x09'] = lambda msg: self._armed(
-            AlarmState.ARMED_SUPPRESSED, msg)
-        self._message_handlers[b'\x0E'] = lambda msg: self._armed(
-            AlarmState.ENTRY_TIME, msg)
-        self._message_handlers[b'\x0F'] = lambda msg: self._armed(
-            AlarmState.EXIT_COUNTDOWN_OVER_10, msg)
-        self._message_handlers[b'\x10'] = lambda msg: self._armed(
-            AlarmState.EXIT_COUNTDOWN_UNDER_10, msg)
-        self._message_handlers[b'\xEF'] = lambda msg: self._command_result(msg)
-        self._message_handlers[b'\x13'] = lambda msg: self._armed(
-            AlarmState.TRIGGERED, msg)
-        self._message_handlers[b'\x14'] = lambda msg: self._armed(
-            AlarmState.TRIGGERED_FIRE, msg)
+        self._message_handlers = {
+            SatelReadCommand.ZONES_VIOLATED: self._zone_violated,
+            SatelReadCommand.PARTITIONS_ARMED_SUPPRESSED: lambda msg: self._armed(
+                AlarmState.ARMED_SUPPRESSED, msg
+            ),
+            SatelReadCommand.PARTITIONS_ARMED_MODE0: lambda msg: self._armed(
+                AlarmState.ARMED_MODE0, msg
+            ),
+            SatelReadCommand.PARTITIONS_ARMED_MODE2: lambda msg: self._armed(
+                AlarmState.ARMED_MODE2, msg
+            ),
+            SatelReadCommand.PARTITIONS_ARMED_MODE3: lambda msg: self._armed(
+                AlarmState.ARMED_MODE3, msg
+            ),
+            SatelReadCommand.PARTITIONS_ENTRY_TIME: lambda msg: self._armed(
+                AlarmState.ENTRY_TIME, msg
+            ),
+            SatelReadCommand.PARTITIONS_EXIT_COUNTDOWN_OVER_10: lambda msg: self._armed(
+                AlarmState.EXIT_COUNTDOWN_OVER_10, msg
+            ),
+            SatelReadCommand.PARTITIONS_EXIT_COUNTDOWN_UNDER_10: lambda msg: self._armed(
+                AlarmState.EXIT_COUNTDOWN_UNDER_10, msg
+            ),
+            SatelReadCommand.PARTITIONS_ALARM: lambda msg: self._armed(
+                AlarmState.TRIGGERED, msg
+            ),
+            SatelReadCommand.PARTITIONS_FIRE_ALARM: lambda msg: self._armed(
+                AlarmState.TRIGGERED_FIRE, msg
+            ),
+            SatelReadCommand.OUTPUTS_STATE: self._output_changed,
+            SatelReadCommand.ARMED_MODE1: lambda msg: self._armed(
+                AlarmState.ARMED_MODE1, msg
+            ),
+            SatelReadCommand.RESULT: lambda msg: self._command_result(msg),
+        }
 
     @property
     def connected(self):
@@ -173,12 +187,12 @@ class AsyncSatel:
 
         try:
             self._reader, self._writer = await asyncio.open_connection(
-                self._host, self._port)
-            _LOGGER.debug("sucess connecting...")
+                self._host, self._port
+            )
+            _LOGGER.debug("success connecting...")
 
         except Exception as e:
-            _LOGGER.warning(
-                "Exception during connecting: %s.", e)
+            _LOGGER.warning("Exception during connecting: %s.", e)
             self._writer = None
             self._reader = None
             return False
@@ -187,8 +201,8 @@ class AsyncSatel:
 
     async def start_monitoring(self):
         """Start monitoring for interesting events."""
-        data = generate_query(
-            b'\x7F\x01\xDC\x99\x80\x00\x04\x00\x00\x00\x00\x00\x00')
+        # TODO: Convert this to enum values
+        data = generate_query(b"\x7f\x01\xdc\x99\x80\x00\x04\x00\x00\x00\x00\x00\x00")
 
         await self._send_data(data)
         resp = await self._read_data()
@@ -197,19 +211,17 @@ class AsyncSatel:
             _LOGGER.warning("Start monitoring - no data!")
             return
 
-        if resp[1:2] != b'\xFF':
+        if int(resp[1:2]) != SatelResultCode.COMMAND_ACCEPTED:
             _LOGGER.warning("Monitoring not accepted.")
 
     def _zone_violated(self, msg):
-
         status = {"zones": {}}
 
         violated_zones = list_set_bits(msg, 32)
         self.violated_zones = violated_zones
         _LOGGER.debug("Violated zones: %s", violated_zones)
         for zone in self._monitored_zones:
-            status["zones"][zone] = \
-                1 if zone in violated_zones else 0
+            status["zones"][zone] = 1 if zone in violated_zones else 0
 
         _LOGGER.debug("Returning status: %s", status)
 
@@ -225,11 +237,13 @@ class AsyncSatel:
 
         output_states = list_set_bits(msg, 32)
         self.violated_outputs = output_states
-        _LOGGER.debug("Output states: %s, monitored outputs: %s",
-                      output_states, self._monitored_outputs)
+        _LOGGER.debug(
+            "Output states: %s, monitored outputs: %s",
+            output_states,
+            self._monitored_outputs,
+        )
         for output in self._monitored_outputs:
-            status["outputs"][output] = \
-                1 if output in output_states else 0
+            status["outputs"][output] = 1 if output in output_states else 0
 
         _LOGGER.debug("Returning status: %s", status)
 
@@ -242,9 +256,9 @@ class AsyncSatel:
         status = {"error": "Some problem!"}
         error_code = msg[1:2]
 
-        if error_code in [b'\x00', b'\xFF']:
+        if error_code in [b"\x00", b"\xff"]:
             status = {"error": "OK"}
-        elif error_code == b'\x01':
+        elif error_code == b"\x01":
             status = {"error": "User code not found"}
 
         _LOGGER.debug("Received error status: %s", status)
@@ -259,7 +273,7 @@ class AsyncSatel:
     #         await asyncio.wait_for(self._command_status_event.wait(),
     #                                timeout=5)
     #     except asyncio.TimeoutError:
-    #         _LOGGER.warning("Timeout waiting for reponse from Satel!")
+    #         _LOGGER.warning("Timeout waiting for response from Satel!")
     #     return self._command_status
 
     async def _send_data(self, data):
@@ -275,8 +289,7 @@ class AsyncSatel:
             self._writer.write(data)
             await self._writer.drain()
         except Exception as e:
-            _LOGGER.warning(
-                "Exception during sending data: %s.", e)
+            _LOGGER.warning("Exception during sending data: %s.", e)
             self._writer = None
             self._reader = None
             return False
@@ -285,13 +298,15 @@ class AsyncSatel:
         """Send arming command to the alarm. Modes allowed: from 0 till 3."""
         _LOGGER.debug("Sending arm command, mode: %s!", mode)
         while len(code) < 16:
-            code += 'F'
+            code += "F"
 
         code_bytes = bytearray.fromhex(code)
-        mode_command = 0x80 + mode
-        data = generate_query(mode_command.to_bytes(1, 'big')
-                              + code_bytes
-                              + partition_bytes(partition_list))
+        mode_command = SatelWriteCommand.ARM_MODE_0 + mode
+        data = generate_query(
+            mode_command.to_bytes(1, "big")
+            + code_bytes
+            + partition_bytes(partition_list)
+        )
 
         await self._send_data(data)
 
@@ -299,12 +314,15 @@ class AsyncSatel:
         """Send command to disarm."""
         _LOGGER.info("Sending disarm command.")
         while len(code) < 16:
-            code += 'F'
+            code += "F"
 
         code_bytes = bytearray.fromhex(code)
 
-        data = generate_query(b'\x84' + code_bytes
-                              + partition_bytes(partition_list))
+        data = generate_query(
+            SatelWriteCommand.DISARM.to_bytes(1, "big")
+            + code_bytes
+            + partition_bytes(partition_list)
+        )
 
         await self._send_data(data)
 
@@ -312,12 +330,15 @@ class AsyncSatel:
         """Send command to clear the alarm."""
         _LOGGER.info("Sending clear the alarm command.")
         while len(code) < 16:
-            code += 'F'
+            code += "F"
 
         code_bytes = bytearray.fromhex(code)
 
-        data = generate_query(b'\x85' + code_bytes
-                              + partition_bytes(partition_list))
+        data = generate_query(
+            SatelWriteCommand.CLEAR_ALARM.to_bytes(1, "big")
+            + code_bytes
+            + partition_bytes(partition_list)
+        )
 
         await self._send_data(data)
 
@@ -330,20 +351,21 @@ class AsyncSatel:
               checked by observe the system state """
         _LOGGER.debug("Turn on, output: %s, code: %s", output_id, code)
         while len(code) < 16:
-            code += 'F'
+            code += "F"
 
         code_bytes = bytearray.fromhex(code)
-        mode_command = 0x88 if state else 0x89
-        data = generate_query(mode_command.to_bytes(1, 'big') +
-                              code_bytes +
-                              output_bytes(output_id))
+        mode_command = (
+            SatelWriteCommand.OUTPUT_ON if state else SatelWriteCommand.OUTPUT_OFF
+        )
+        data = generate_query(
+            mode_command.to_bytes(1, "big") + code_bytes + output_bytes(output_id)
+        )
         await self._send_data(data)
 
     def _armed(self, mode, msg):
         partitions = list_set_bits(msg, 4)
 
-        _LOGGER.debug("Update: list of partitions in mode %s: %s",
-                      mode, partitions)
+        _LOGGER.debug("Update: list of partitions in mode %s: %s", mode, partitions)
 
         self.partition_states[mode] = partitions
 
@@ -355,7 +377,7 @@ class AsyncSatel:
             return []
 
         try:
-            data = await self._reader.readuntil(b'\xFE\x0D')
+            data = await self._reader.readuntil(b"\xfe\x0d")
             _LOGGER.debug("-- Receiving data --")
             print_hex(data)
             _LOGGER.debug("-- ------------- --")
@@ -363,8 +385,8 @@ class AsyncSatel:
 
         except Exception as e:
             _LOGGER.warning(
-                "Got exception: %s. Most likely the other side has "
-                "disconnected!", e)
+                "Got exception: %s. Most likely the other side has disconnected!", e
+            )
             self._writer = None
             self._reader = None
 
@@ -382,7 +404,7 @@ class AsyncSatel:
             if self.closed:
                 return
             # Command to read status of the alarm
-            data = generate_query(b'\xEE\x01\x01')
+            data = generate_query(b"\xee\x01\x01")
             await self._send_data(data)
 
     async def _update_status(self):
@@ -399,16 +421,19 @@ class AsyncSatel:
             return
 
         msg_id = resp[0:1]
-        str_msg_id = ''.join(format(x, '02x') for x in msg_id)
+        str_msg_id = "".join(format(x, "02x") for x in msg_id)
         if msg_id in self._message_handlers:
             _LOGGER.info("Calling handler for id: 0x%s", str_msg_id)
             self._message_handlers[msg_id](resp)
         else:
             _LOGGER.info("Ignoring message: 0x%s", str_msg_id)
 
-    async def monitor_status(self, alarm_status_callback=None,
-                             zone_changed_callback=None,
-                             output_changed_callback=None):
+    async def monitor_status(
+        self,
+        alarm_status_callback=None,
+        zone_changed_callback=None,
+        output_changed_callback=None,
+    ):
         """Start monitoring of the alarm status.
 
         Send command to satel integra to start sending updates. Read in a
@@ -448,26 +473,3 @@ class AsyncSatel:
         self.closed = True
         if self.connected:
             self._writer.close()
-
-
-def demo(host, port):
-    """Basic demo of the monitoring capabilities."""
-    # logging.basicConfig(level=logging.DEBUG)
-
-    loop = asyncio.get_event_loop()
-    stl = AsyncSatel(host,
-                     port,
-                     loop,
-                     [1, 2, 3, 4, 5, 6, 7, 8, 12, 13, 14, 15, 16, 17, 18, 19,
-                      20, 21, 22, 23, 25, 26, 27, 28, 29, 30],
-                     [8, 9, 10]
-                     )
-
-    loop.run_until_complete(stl.connect())
-    loop.create_task(stl.arm("3333", (1,)))
-    loop.create_task(stl.disarm("3333",(1,)))
-    loop.create_task(stl.keep_alive())
-    loop.create_task(stl.monitor_status())
-
-    loop.run_forever()
-    loop.close()
